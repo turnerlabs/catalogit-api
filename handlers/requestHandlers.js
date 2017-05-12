e1f4efb10c3f3c549cabeef378bd123aaf8c40fc
Wilson Wise <wilson.wise@turner.com>
2017-05-12 12:15:27 -0400

    fix(whitespace): consistent use of whitespace

--- handlers/requestHandlers.js
+++ handlers/requestHandlers.js
@@ -1,47 +1,47 @@
-"use strict"
+"use strict";
 
-let documentToObject = require('../restit');
+module.exports = function (lm) {
+    let m = lm,
+        e = {};
 
-module.exports = function(lm) {
+    e.getContainers = function (req, res) {
+        let promises = [];
 
-  let m = lm;
-  let e = {};
+        return m.Container.find({}).sort({name: 1}).exec((err, results) => {
+            if (err) {
+                res.status(500);
+                return res.json({ error: 'Something went wrong' });
+            }
 
-  e.getContainers = function(req, res) {
-    let promises = [];
+            let data = results.map((result) => result.name).filter((result, i , results) => results.indexOf(result) === i);
 
-    return m.Container.find({}).sort({name: 1}).exec(function (err, results) {
-        if(err) {res.status(500);res.send(JSON.stringify({error: 'Something went wrong'})); return};
-        let data = results.map((result) => result.name)
-                          .filter((result, i , results) => results.indexOf(result) === i);
-        res.json(data);
-    });
-  }
+            cached = data;
+            res.json(data);
+        });
+    }
 
-  e.getContainerVersions = function(req, res) {
-      return m.Container.find({name: req.params.name}).exec(function (err, container) {
-        if(err || ! container) {
-          res.status(404);
-          res.send(JSON.stringify({error: 'Container not found'}));
-          return;
-        }
+    e.getContainerVersions = function (req, res) {
+        return m.Container.find({ name: req.params.name }).exec((err, container) => {
+            if (err || ! container) {
+                res.status(404);
+                return res.json({ error: 'Container not found' });
+            }
 
-        let data = container.map((data) => ({version: data.version, name: data.name, image: data.image}));
-        res.json(data);
-      });
-  }
+            let data = container.map((data) => { version: data.version, name: data.name, image: data.image });
+            res.json(data);
+        });
+    }
 
-  e.getContainer = function(req, res) {
-      return m.Container.findOne({name: req.params.name, version: req.params.version}).exec(function (err, container) {
-        if(err || ! container) {
-          res.status(404);
-          res.send(JSON.stringify({error: 'Container not found'}));
-          return;
-        }
+    e.getContainer = function (req, res) {
+        return m.Container.findOne({ name: req.params.name, version: req.params.version }).exec((err, container) => {
+            if (err || ! container) {
+                res.status(404);
+                return res.json({ error: 'Container not found' });
+            }
 
-        res.json({name: container.name, version: container.version, image: container.image});
-      });
-  }
+            res.json({ name: container.name, version: container.version, image: container.image });
+        });
+    }
 
-  return e;
+    return e;
 };
