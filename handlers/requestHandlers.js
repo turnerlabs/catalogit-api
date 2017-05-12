7adefdc532fbe64c563600339d44ffbfd13abece
Wilson Wise <wilson.wise@turner.com>
2017-05-12 13:05:15 -0400

    fix(cache): return early to elminate need for else

--- handlers/requestHandlers.js
+++ handlers/requestHandlers.js
@@ -12,25 +12,25 @@ module.exports = function (lm) {
 
         if (cached) {
             console.log('using cached response');
-            res.json(cached);
-        } else {
-            return m.Container.find({}).sort({name: 1}).exec((err, results) => {
-                if (err) {
-                    res.status(500);
-                    return res.json({ error: 'Something went wrong' });
-                }
-
-                let data = results.map((result) => result.name).filter((result, i , results) => results.indexOf(result) === i);
-
-                cached = data;
-                res.json(data);
-            });
+            return res.json(cached);
         }
+
+        return m.Container.find({}).sort({name: 1}).exec((err, results) => {
+            if (err) {
+                res.status(500);
+                return res.json({ error: 'Something went wrong' });
+            }
+
+            let data = results.map((result) => result.name).filter((result, i , results) => results.indexOf(result) === i);
+
+            cached = data;
+            res.json(data);
+        });
     }
 
     e.getContainerVersions = function (req, res) {
         return m.Container.find({ name: req.params.name }).exec((err, container) => {
-            if (err || ! container) {
+            if (err || !container) {
                 res.status(404);
                 return res.json({ error: 'Container not found' });
             }
@@ -42,7 +42,7 @@ module.exports = function (lm) {
 
     e.getContainer = function (req, res) {
         return m.Container.findOne({ name: req.params.name, version: req.params.version }).exec((err, container) => {
-            if (err || ! container) {
+            if (err || !container) {
                 res.status(404);
                 return res.json({ error: 'Container not found' });
             }
